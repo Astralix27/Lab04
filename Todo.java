cf045ae59f52b8669fa74c61cf53fc8c656f6258
Astralix27 <47227934+Astralix27@users.noreply.github.com>
2019-02-12 14:40:33 -0600

    Finished the boolean equals for Animal

---
+++ Todo.java
@@ -0,0 +1,18 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-10-02
+OU ID_______________: 113465328
+OU EMAIL____________: Kennen.F.Reyes-1@ou.edu
+
+Tasks:
+1. Code everything
+2. Test everything
+
+Estimated Time (minutes):
+1. 60
+2. 90
+
+Actual Time (minutes):
+1. 75
+2. 90
+*/}
