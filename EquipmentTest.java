b471e8ca509f951bb406a1474ae60af4da4b0310
Astralix27 <47227934+Astralix27@users.noreply.github.com>
2019-02-12 15:01:35 -0600

    Finished and tested the EquipmentTest

--- EquipmentTest.java
+++ EquipmentTest.java
@@ -1,4 +1,22 @@
+
+
 public class EquipmentTest
 {
-
+	public void testConstructorParams() throws AssertException
+	{
+		Equipment equipment = new Equipment("Smoke Bomb/5,5.0,15.75,A getaway tool");
+		
+		Assert.assertEquals("Smoke Bomb", equipment.getName());
+		Assert.assertEquals(5, equipment.getCount(), 0.01);
+		Assert.assertEquals(5.0, equipment.getTotalWeight(), 0.01);
+		Assert.assertEquals(15.75, equipment.getTotalPrice(), 0.01);
+		Assert.assertEquals("A getaway tool", equipment.getDescription());
+	}
+	
+	public void testToString() throws AssertException
+	{
+    	Equipment equipment = new Equipment("Smoke Bomb/5,5.0,15.75,A getaway tool");
+    	
+    	Assert.assertEquals("Name: Smoke Bomb, Number: 5, Weight: 5.00 lbs, Price: $15.75 - A getaway tool", equipment.toString());
+	}
 }
