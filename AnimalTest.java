68f4a45defb11392791a06b596fc46b545e5d927
Astralix27 <47227934+Astralix27@users.noreply.github.com>
2019-02-12 14:56:00 -0600

    Constructed, finished, and tested AnimalTest

--- AnimalTest.java
+++ AnimalTest.java
@@ -1,3 +1,5 @@
+
+
 /**
  * Testing class for Product object
  *
@@ -12,7 +14,7 @@ public class AnimalTest
      */
     public void emptyConstructorTest() throws AssertException
     {
-        // Use the default constructor
+		// Use the default constructor
         Animal animal = new Animal();
 
         // The name should be "noname", color "unknown", height and weight 0.
@@ -23,5 +25,21 @@ public class AnimalTest
     }
 
     // TODO: test full constructor, getters, and toString
+    public void testConstructorParams() throws AssertException
+    {
+    	Animal animal = new Animal("Orange", "Tony", 200.1, 20.0);
+    	
+    	Assert.assertEquals("Tony", animal.getName());
+    	Assert.assertEquals("Orange", animal.getColor());
+    	Assert.assertEquals(200.1, animal.getWeight(), 0.01);
+    	Assert.assertEquals(20.0, animal.getHeight(), 0.01);
+    }
+    
+    public void testToString() throws AssertException
+    {
+    	Animal animal = new Animal("White", "Bonny", 45.0, 5.6);
+    	
+    	Assert.assertEquals("Bonny, a White-colored animal. 45.0 pounds, 5.6 inches\n", animal.toString());
+    }
 }
 
